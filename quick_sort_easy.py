159d4e7cda8d0e8f889af708d6e59d456e0ff43d
Dustin Lo <dustinlo@Dustins-MacBook-Pro.local>
2018-09-19 11:46:42 -0700

    added notes to quick_sort_easy.py

--- quick_sort_easy.py
+++ quick_sort_easy.py
@@ -1,29 +1,33 @@
 
 def quick_sort(ls):
-    if len(ls) <= 1:
+    if len(ls) <= 1: # stops recursion
         return ls
 
-    if len(ls) == 2:
+    if len(ls) == 2: # stops recursion, but also sorts it because sorting list of length 2 is easy
         if ls[0] > ls[1]:
             ls.reverse()
         return ls
 
     print ls
 
-    pivot_idx = len(ls)/2 #- 1
-    pivot_value = ls[pivot_idx]
+    pivot_idx = len(ls)/2
+    pivot_value = ls[pivot_idx] # choosing pivot value at middle of list
     
-    left_ls = filter(lambda x: x < pivot_value, ls)
-    left_ls.append(pivot_value)
-    right_ls = filter(lambda x: x > pivot_value, ls)
+    left_ls = filter(lambda x: x < pivot_value, ls) # all values less than pivot are to the left
+    left_ls.append(pivot_value) # keep the pivot value at the end
+    right_ls = filter(lambda x: x > pivot_value, ls) # all values greater than pivot are to the right
 
     print '{} {}'.format(left_ls, right_ls)
 
+    # recursive step
+    # list less than pivot goes through the same step until it reaches a list of length 1 or 2 (easy to sort)
     left_ls = quick_sort(left_ls)
+    # list greater than pivot goes through the same step
     right_ls = quick_sort(right_ls)
 
     print '{} {}'.format(left_ls, right_ls)
 
+    # once both lists and sorted we can combine them to create a larger sorted list
     return left_ls + right_ls
     
 
@@ -32,4 +36,4 @@ if __name__ == '__main__':
     x = np.arange(21)
     np.random.shuffle(x)
     print x.tolist()
-    quick_sort(x.tolist())
+    print quick_sort(x.tolist())
