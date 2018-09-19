86153ca94b14905aec819ce3c7f93ed337210249
Dustin Lo <dustinlo@Dustins-MacBook-Pro.local>
2018-09-19 11:40:41 -0700

    added comments to merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,40 +1,45 @@
 
 
 def merge(ls1, ls2):
-	# print("\nlist inputs to merge: {} {}".format(ls1, ls2))
-	ordered_list = []
-	
-	i = 0 # counter for ls1
-	j = 0 # counter for ls2
-	while(i < len(ls1) and j < len(ls2)):
-		# do stuff until one of them runs out
-		if(ls1[i] < ls2[j]):
-			ordered_list = ordered_list + [ls1[i]]
-			i += 1
-		else:
-			ordered_list = ordered_list + [ls2[j]]
-			j += 1
-	
-	if(i < len(ls1)):
-		leftover = ls1[i:]
-	else:
-		leftover = ls2[j:]
+    ordered_list = []
+    
+    i = 0 # counter for ls1
+    j = 0 # counter for ls2
+    while(i < len(ls1) and j < len(ls2)):
+        # adds values in combination list until one of the lists runs out
+        if(ls1[i] < ls2[j]):
+            ordered_list = ordered_list + [ls1[i]]
+            i += 1
+        else:
+            ordered_list = ordered_list + [ls2[j]]
+            j += 1
+    
+    if(i < len(ls1)):
+        leftover = ls1[i:]
+    else:
+        leftover = ls2[j:]
 
-	return ordered_list + leftover
+    # leftover value is already sorted so we can add them together
+    return ordered_list + leftover
 
 
 def merge_sort(ls):
-	if len(ls) == 1:
-		return ls
+    if len(ls) == 1:
+        # stops the recursion at list length 1
+        # lists of length 1 are technically sorted
+        return ls
 
-	mid = len(ls)/2
-	array_one = ls[:mid]
-	array_two = ls[mid:]
+    # splitting the list in half
+    mid = len(ls)/2
+    array_one = ls[:mid]
+    array_two = ls[mid:]
 
-	array_one = merge_sort(array_one)
-	array_two = merge_sort(array_two)
+    # recursive step to further break down the lists in half until it reaches a list of length 1
+    array_one = merge_sort(array_one)
+    array_two = merge_sort(array_two)
 
-	return merge(array_one, array_two)
+    # returns the merged list up one level
+    return merge(array_one, array_two)
 
 
 if __name__ == '__main__':
@@ -42,4 +47,4 @@ if __name__ == '__main__':
     x = np.arange(21)
     np.random.shuffle(x)
     print x.tolist()
-    quick_sort(x.tolist())
+    print merge_sort(x.tolist())
