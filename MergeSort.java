f266a49adb30fded3f0d3a4df70e73b3c482b40b
Dustin Lo <dustinlo@users-MacBook-Pro-2.local>
2018-11-21 00:31:48 -0800

    separated merge into 2 different functions

--- MergeSort.java
+++ MergeSort.java
@@ -2,20 +2,12 @@ import java.util.Arrays;
 
 class MergeSort {
 
-    public static int[] mergeSort(int[] ls) {
-        if(ls.length == 1) {
-            return ls;
-        }
-        int len = ls.length;
-        int[] left = Arrays.copyOfRange(ls, 0, len/2);
-        int[] right = Arrays.copyOfRange(ls, len/2, len);
-        left = mergeSort(left); // recursive step, break it down until array of length 1
-        right = mergeSort(right); // recursive step, break it down until array of length 1
+    public static int[] mergeStep(int[] left, int[] right) {
 
         int i = 0; // array index for left
         int j = 0; // array index for right
         int k = 0; // array index for sorted
-        int[] sorted = new int[len];
+        int[] sorted = new int[left.length + right.length];
         int[] leftOver;
         while(i < left.length && j < right.length) {
             if(left[i] < right[j]) {
@@ -38,15 +30,29 @@ class MergeSort {
             sorted[k] = leftOver[n];
             k++;
         }
-
         return sorted;
     }
 
+    public static int[] mergeSort(int[] ls) {
+        if(ls.length == 1) {
+            return ls;
+        }
+        int len = ls.length;
+        int[] left = Arrays.copyOfRange(ls, 0, len/2);
+        int[] right = Arrays.copyOfRange(ls, len/2, len);
+        left = mergeSort(left); // recursive step, break it down until array of length 1
+        right = mergeSort(right); // recursive step, break it down until array of length 1
+
+        // each recursive level starting from array length 1 up
+        // will combine the 2 ordered arrays into one larger ordered array
+        return mergeStep(left, right);
+    }
+
     public static void main(String args[]) {
-        // MergeSort test = new MergeSort();
         int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,12,11,10,9,8,7,6,5,4,3,2,1};
         System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(mergeSort(arr)));
+        int[] sortedArray = mergeSort(arr);
+        System.out.println(Arrays.toString(sortedArray));
     }
 
 }
