8a0547b583aad80d8b59b5d688a220dbfe502f3b
Dustin Lo <dustinlo@users-mbp-2.lan>
2019-02-11 19:56:42 -0800

    reverse integer

--- leetcode/q7_reverse_integer.py
+++ leetcode/q7_reverse_integer.py
@@ -8,15 +8,21 @@ def reverse(x):
     	x = x * -1
     	neg_sign = -1
 
+    if x > 2147483647:
+        return 0
+
     while(True):
     	y = (x%digit - x%(digit/10)) / (digit/10)
     	ans = ans * 10 + y
-    	print("y: {} \t ans: {}".format(y, ans))
+    	# print("y: {} \t ans: {}".format(y, ans))
     	
     	if(x / digit == 0):
     		break
     	
     	digit = digit * 10
+
+    if ans > 2147483647:
+        return 0 
 
     return ans * neg_sign
 
@@ -25,4 +31,13 @@ if __name__ == '__main__':
     print(reverse(-123450))
     print('')
     print(reverse(123))
+    print('')
     print(reverse(10000001))
+    print('')
+    print(reverse(13))
+    print('')
+    print(reverse(10))
+    print('')
+    print(reverse(7))
+    print('')
+    print(reverse(1534236469))
