7a675c2f8bdf6a3656eeb6afcb21a3d3d09f26a6
Dustin Lo <dustinlo@Dustins-MacBook-Pro.local>
2019-02-11 18:01:34 -0800

    q7

---
+++ leetcode/q7_reverse_integer.py
@@ -0,0 +1,28 @@
+def reverse(x):
+    print("x: {}".format(x))
+    digit = 10
+    ans = 0
+
+    neg_sign = 1
+    if x < 0:
+    	x = x * -1
+    	neg_sign = -1
+
+    while(True):
+    	y = (x%digit - x%(digit/10)) / (digit/10)
+    	ans = ans * 10 + y
+    	print("y: {} \t ans: {}".format(y, ans))
+    	
+    	if(x / digit == 0):
+    		break
+    	
+    	digit = digit * 10
+
+    return ans * neg_sign
+
+
+if __name__ == '__main__':
+    print(reverse(-123450))
+    print('')
+    print(reverse(123))
+    print(reverse(10000001))
