3b6019c41d9eace7c5fd250dc7a22396c2a49996
Dustin Lo <dustinlo@users-MacBook-Pro-2.local>
2019-02-27 19:01:36 -0800

    perfect square

---
+++ leetcode/q367_perfect_square.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPerfectSquare(self, num):
+        x_n_minus_1 = (float(num) + 1)/2
+        i = 0
+        while i < 20:
+            x_n = x_n_minus_1 - (x_n_minus_1 ** 2 - num) / float(2 * x_n_minus_1)
+            i += 1
+            
+            if abs(x_n - x_n_minus_1) == 0.0:
+                break
+            x_n_minus_1 = x_n
+            # print(x_n)
+         
+        return x_n == int(x_n)
