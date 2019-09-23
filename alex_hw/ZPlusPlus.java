2fbb64266a8e37ffabec75aac656a11e5552be24
DustinKLo <dustinlo.pcc@gmail.com>
2019-09-23 14:16:22 -0700

    alex's hw in java

---
+++ alex_hw/ZPlusPlus.java
@@ -0,0 +1,48 @@
+import java.io.*; 
+import java.util.*; 
+
+public class ZPlusPlus {
+	public static Boolean validate(String inputString) {
+		Stack<Character> stack = new Stack<>();
+		
+		HashMap<Character, Character> mapper = new HashMap<>();
+		mapper.put(')', '(');
+		mapper.put(']', '[');
+		mapper.put('}', '{');
+
+		for (int i = 0; i < inputString.length(); i++) {
+		    char c = inputString.charAt(i);        
+		    if (c == '(' || c == '[' || c == '{') {
+		    	stack.push(c);
+		    } else {
+		    	if (mapper.get(c) == stack.peek()) {
+		    		stack.pop();
+		    	} else {
+		    		return false;
+		    	}
+		    }
+		}
+		return (stack.size() == 0) ? true : false;
+	}
+
+	public static void main(String[] args) {
+		String x = "[]{}()";
+		System.out.printf("%s: %b\n", x, validate(x));
+
+		x = "{([])}";
+		System.out.printf("%s: %b\n", x, validate(x));
+
+		x = "()[{}]";
+		System.out.printf("%s: %b\n", x, validate(x));
+
+		x = "[{}()]";
+		System.out.printf("%s: %b\n", x, validate(x));
+
+		x = "[({)}]";
+		System.out.printf("%s: %b\n", x, validate(x));
+
+		x = "()()()()()()((()))()()(((())))(())";
+		System.out.printf("%s: %b\n", x, validate(x));
+
+	}
+}
