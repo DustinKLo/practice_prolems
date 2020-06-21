a349a7b5d4dbe2a1e872b0365d6c15929b8f2b0a
DustinKLo <dustinlo.pcc@gmail.com>
2020-06-21 09:20:53 -0700

    binary searchh

---
+++ general/binary_search.py
@@ -0,0 +1,37 @@
+
+def binary_search(arr, target):
+	def traverse(target, left, right):
+		if left > right:
+			return -1
+		
+		mid = int((left + right) / 2)
+		print(left, mid, right)
+		
+		if arr[mid] == target:
+			return mid
+
+		if arr[mid] < target:
+			return 0 + traverse(target, mid + 1, right)
+		else:
+			return 0 + traverse(target, left, mid - 1)
+	
+	idx = traverse(target, 0, len(arr) - 1)
+	print("ans: ", idx)
+	return idx
+
+
+if __name__ == '__main__':
+	test_arr = [0,1,2,3,4,5]
+	binary_search(test_arr, 3)
+	print("#################\n")
+	binary_search(test_arr, 0)
+	print("#################\n")
+	binary_search(test_arr, 5)
+	print("#################\n")
+	binary_search(test_arr, 6)
+	print("#################\n")
+	binary_search(test_arr, -100)
+	print("#################\n")
+	binary_search(list(range(1000)), 666)
+	print("#################\n")
+	binary_search(list(range(1999999)), 333)
