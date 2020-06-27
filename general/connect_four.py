08a7de7ff3b93b2fead69c60a8a79e507ca04104
DustinKLo <dustinlo.pcc@gmail.com>
2020-06-27 10:13:48 -0700

    added replay, score tracker and pattern tracker in board

--- general/connect_four.py
+++ general/connect_four.py
@@ -18,7 +18,7 @@ class ConnectFour:
         self.buffer = 3
         self.buffer_space = ' ' * self.buffer
         
-        self.index_tracker = [5, 5, 5, 5, 5, 5, 5]
+        self.column_spaces = [5, 5, 5, 5, 5, 5, 5]
         self.board = [
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
@@ -27,6 +27,7 @@ class ConnectFour:
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ]
         ]
+        self.winning_pattern = []
 
     def display(self):
         print("\n")
@@ -40,11 +41,18 @@ class ConnectFour:
         print("\n")
 
     def add(self, column, symbol):
-        # check self.index_tracker to see if column is > 0
+        # check self.column_spaces to see if column is > 0
         column -= 1
-        self.index_tracker[column] -= 1
-        row_num = self.index_tracker[column]
+        self.column_spaces[column] -= 1
+        row_num = self.column_spaces[column]
         self.board[row_num + 1][column] = symbol
+
+    def clear_game(self):
+        self.column_spaces = [5] * 7
+        self.winning_pattern = []
+        for i in range(6):
+            for j in range(7):
+                self.board[i][j] = ' '
 
     def input_handler(self, player):
         """
@@ -61,45 +69,48 @@ class ConnectFour:
             if column < 1 or column  > 7:
                 print("choose between column 1 and 7")
                 continue
-            if self.index_tracker[column - 1] < 0:
+            if self.column_spaces[column - 1] < 0:
                 print("Column %d is full" % column)
                 continue
             else:
                 return column
 
-    def check_horizontal(self, symbol, count, y, x):
+    def check_horizontal(self, symbol, count, y, x, pattern):
         # y is row, x is column
         if x > 6 or y > 5:  # if outside board boundaries
             return False
         if self.board[y][x] != symbol:
             return False
         if count == 3 and self.board[y][x] == symbol:
+            self.set_winning_pattern(pattern + [[y, x]])
             return True
-        return self.check_horizontal(symbol, count + 1, y, x + 1)
+        return self.check_horizontal(symbol, count + 1, y, x + 1, pattern + [[y, x]])
 
-    def check_vertical(self, symbol, count, y, x):
+    def check_vertical(self, symbol, count, y, x, pattern):
         # y is row, x is column
         if x > 6 or y > 5:  # if outside board boundaries
             return False
         if self.board[y][x] != symbol:
             return False
         if count == 3 and self.board[y][x] == symbol:
+            self.set_winning_pattern(pattern + [[y, x]])
             return True
-        return self.check_vertical(symbol, count + 1, y - 1, x)
+        return self.check_vertical(symbol, count + 1, y - 1, x, pattern + [[y, x]])
 
 
-    def check_diagonal(self, symbol, count, y, x, direction):
+    def check_diagonal(self, symbol, count, y, x, direction, pattern):
         if x > 6 or y > 5:  # if outside board boundaries
             return False
         if self.board[y][x] != symbol:
             return False
         if count == 3 and self.board[y][x] == symbol:
+            self.set_winning_pattern(pattern + [[y, x]])
             return True
         if direction == 1:
             # diagonal going down
-            return self.check_diagonal(symbol, count + 1, y + 1, x + 1, direction)
+            return self.check_diagonal(symbol, count + 1, y + 1, x + 1, direction, pattern + [[y, x]])
         # diagonal going up
-        return self.check_diagonal(symbol, count + 1, y - 1, x + 1, direction)
+        return self.check_diagonal(symbol, count + 1, y - 1, x + 1, direction, pattern + [[y, x]])
 
     def detect_winner(self, player):
         """
@@ -112,15 +123,22 @@ class ConnectFour:
                 if self.board[i][j] == ' ':
                     continue
                 else:
-                    winner = self.check_horizontal(player.symbol, 0, i, j) or \
-                             self.check_vertical(player.symbol, 0, i, j) or \
-                             self.check_diagonal(player.symbol, 0, i, j, 1) or \
-                             self.check_diagonal(player.symbol, 0, i, j, -1)
+                    winner = self.check_horizontal(player.symbol, 0, i, j, []) or \
+                             self.check_vertical(player.symbol, 0, i, j, []) or \
+                             self.check_diagonal(player.symbol, 0, i, j, 1, []) or \
+                             self.check_diagonal(player.symbol, 0, i, j, -1, [])
                 if winner is True:
                     break
             if winner is True:
                 break
         return winner
+
+    def set_winning_pattern(self, pattern):
+        print(pattern)
+        for sq in pattern:
+            y = sq[0]
+            x = sq[1]
+            self.board[y][x] = "█"
 
     def check_stalemate(self):
         for row in self.board:
@@ -128,6 +146,12 @@ class ConnectFour:
                 if col == ' ':
                     return False
         return True
+
+    def display_score(self, player1, player2):
+        print("\n")
+        print(" " * 3 + "Player 1 (%s) score: %d" % (player1.symbol, player1.get_total_wins()))
+        print(" " * 3 + "Player 2 (%s) score: %d" % (player2.symbol, player2.get_total_wins()))
+        print("\n")
 
 
 class Player:
@@ -142,19 +166,13 @@ class Player:
         return self._win_count
 
 
-if __name__ == '__main__':
-    PLAYER_1_SYMBOL = 'X'
-    PLAYER_2_SYMBOL = 'O'
-    
-    player1 = Player(PLAYER_1_SYMBOL)
-    player2 = Player(PLAYER_2_SYMBOL)
-    
-    game = ConnectFour()
+
+def game_handler(game, player1, player2):
     turn = -1
 
     while True:
         current_player = player1 if turn == -1 else player2
-        symbol = PLAYER_1_SYMBOL if turn == -1 else PLAYER_2_SYMBOL
+        symbol = player1.symbol if turn == -1 else player2.symbol
         
         game.display()
         column_input = game.input_handler(current_player)
@@ -167,10 +185,33 @@ if __name__ == '__main__':
             print("*" * 50)
             print("WINNER %s!" % current_player.symbol)
             print("*" * 50)
+            current_player.increment_win()
+            game.display_score(player1, player2)
             break
         if game.check_stalemate() is True:
             print("NO WINNER, STALEMATE")
             break
         turn *= -1
-    
     game.display()
+
+
+if __name__ == '__main__':
+    PLAYER_1_SYMBOL = 'X'
+    PLAYER_2_SYMBOL = 'O'
+    player1 = Player(PLAYER_1_SYMBOL)
+    player2 = Player(PLAYER_2_SYMBOL)
+
+    connect_four = ConnectFour()
+
+    while True:
+        game_handler(connect_four, player1, player2)
+
+        replay = input("\nReplay round? y/N: ")
+        replay = replay.strip()
+
+        if replay.lower() == 'y':
+            connect_four.clear_game()
+            clear_screen()
+            continue
+        else:
+            break
