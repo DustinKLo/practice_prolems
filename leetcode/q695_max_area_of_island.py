0f78883062601dcb814da88d265303f882aef82b
DustinKLo <dustin.k.lo@jpl.nasa.gpv>
2020-07-05 01:44:58 -0700

    code cleanup for q695

--- leetcode/q695_max_area_of_island.py
+++ leetcode/q695_max_area_of_island.py
@@ -17,38 +17,32 @@ class Solution(object):
         def traverse(y, x):
             # y rows, x columns
             if y < 0 or y > len(grid) - 1:
-                return
+                return 0
             if x < 0 or x > len(grid[y]) - 1:
-                return
+                return 0
 
-            if grid[y][x] == 0:
-                return
+            if grid[y][x] == 0 or visited[y][x] == 1:
+                return 0  # is water or already visited
 
-            if visited[y][x] == 1:
-                return
-
-            self.current_area += 1
             visited[y][x] = 1  # so we don't revisit this
             self.island_coords.add((y, x))
 
-            traverse(y + 1, x)  # up
-            traverse(y - 1, x)  # down
-            traverse(y, x - 1)  # left
-            traverse(y, x + 1)  # right
+            # traverse up down left right
+            island_area = traverse(y + 1, x) + traverse(y - 1, x) + \
+                          traverse(y, x - 1) + traverse(y, x + 1)
+            return island_area + 1  # adding 1 if it reaches the end and is an island
 
         for j in range(len(grid)):  # rows
             for i in range(len(grid[j])):  # columns
-                if grid[j][i] == 0:
+                if grid[j][i] == 0 or visited[j][i] == 1:
                     continue
 
                 self.island_coords = set()
-                self.current_area = 0
+                current_area = traverse(j, i)
 
-                traverse(j, i)
-
-                if self.current_area > self.max_area:
+                if current_area > self.max_area:
                     largest_island_coords = self.island_coords
-                    self.max_area = self.current_area
+                    self.max_area = current_area
 
         for _y, _x in largest_island_coords:
             grid[_y][_x] = '\033[1m' + '1' + '\033[0m'
