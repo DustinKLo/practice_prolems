da0947f66b33c0b852770602eb59f1211437646b
DustinKLo <dustin.k.lo@jpl.nasa.gpv>
2020-07-24 10:51:07 -0700

    q103 BFS

--- leetcode/q103_binary_tree_zigzag_order.py
+++ leetcode/q103_binary_tree_zigzag_order.py
@@ -12,45 +12,63 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        depths = []
+        def dfs(node, depth, order):
+            if node is None:
+                return
+            
+            if depth > len(depths) - 1:
+                depths.append([node.val])
+            else:
+                if order == 1:
+                    depths[depth].append(node.val)
+                else:
+                    depths[depth].insert(0, node.val)
+            dfs(node.left, depth + 1, order * -1)
+            dfs(node.right, depth + 1, order * -1)
+        
+        dfs(root, 0, 1)
+        for d in depths:
+            print(d)
+        print("##################\n")
+        return depths
+
+    def zigzagLevelOrderBFS(self, root):
         if root is None:
             return []
-        
-        queue = []
-        queue.append(root)  # enqueue
-        
-        levels = []
+
+        cur_row = [root]
+        next_row = []
+
+        rows = []
         order = 1
-        
-        while len(queue) > 0:
-            queue_len = len(queue)
-            level = []
 
-            while queue_len > 0:
+        while len(cur_row) > 0:
+            cur_row_len = len(cur_row)
+            row = []
+
+            while cur_row_len > 0:
+                node = cur_row.pop(-1)
+                row.append(node.val)
+
                 if order == 1:
-                    node = queue.pop(0)
+                    next_row.append(node.left) if node.left else None
+                    next_row.append(node.right) if node.right else None
                 else:
-                    node = queue.pop(-1)
-                level.append(node.val)
+                    next_row.append(node.right) if node.right else None
+                    next_row.append(node.left) if node.left else None
 
-                if order == -1:
-                    if node.left:
-                        queue.insert(0, node.left)
-                    if node.right:
-                        queue.insert(0, node.right)
-                else:
-                    if node.right:
-                        queue.insert(0, node.right)
-                    if node.left:
-                        queue.insert(0, node.left)
-                queue_len -= 1
+                cur_row_len -= 1
 
-            levels.append(level)
+            rows.append(row)
+            cur_row = next_row
+            next_row = []
             order *= -1
 
-        for l in levels:
-            print(l)
-        print("########################\n")
-        return levels
+        for r in rows:
+            print(r)
+        print("##################\n")
+        return rows
 
 
 if __name__ == '__main__':
@@ -62,6 +80,7 @@ if __name__ == '__main__':
     root.right.left = TreeNode(15)
     root.right.right = TreeNode(7)
     s.zigzagLevelOrder(root)
+    s.zigzagLevelOrderBFS(root)
 
     root = TreeNode(1)
     root.left = TreeNode(2)
@@ -69,3 +88,4 @@ if __name__ == '__main__':
     root.right = TreeNode(3)
     root.right.right = TreeNode(5)
     s.zigzagLevelOrder(root)
+    s.zigzagLevelOrderBFS(root)
